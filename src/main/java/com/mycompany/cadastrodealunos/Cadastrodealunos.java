39f46ad238ab11662583e4e42d5efadd1c9f9b80
Lucaseduardo1895 <lucaseduardo1895@gmail.com>
2023-06-13 19:24:36 -0300

    cadastro de alunos

---
+++ src/main/java/com/mycompany/cadastrodealunos/Cadastrodealunos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cadastrodealunos;
+
+/**
+ *
+ * @author aluno
+ */
+public class Cadastrodealunos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
